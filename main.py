00d2cbf1628a0647ebe7bba18c786bf0830ec503
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-09-01 16:35:27 -0600

    Its finally working.

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen Width: {SCREEN_WIDTH}")
+    print(f"Screen_height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+     main()
