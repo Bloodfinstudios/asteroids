e6154b2f5f247d625859a48ba1f716a06854813d
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-09-01 17:22:08 -0600

    The game window is now working.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pygame
 from constants import *
 
+pygame.init()
+pygame.mixer.quit()
+
 def main():
     print("Starting Asteroids!")
-    print(f"Screen Width: {SCREEN_WIDTH}")
-    print(f"Screen_height: {SCREEN_HEIGHT}")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+# Set up the display outside main()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("My Pygame Window")
 
 if __name__ == "__main__":
-     main()
+    main()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
