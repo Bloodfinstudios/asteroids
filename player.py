b24526c2a998bd489dd156ce229d7feabf4875fd
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-09-02 21:17:29 -0600

    All charchater movement is now added.

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_MOVEMENT
 import pygame
 
 class Player(CircleShape):
@@ -27,3 +27,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        disp = forward * PLAYER_MOVEMENT * dt
+
+        if keys[pygame.K_w]:
+            self.position += disp
+        if keys[pygame.K_s]:
+            self.position -= disp 
+
