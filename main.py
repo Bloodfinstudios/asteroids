0f5d6ff4c80be2cf736f0855aceac801d4caeb97
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-09-06 17:13:09 -0600

    Collision is now working, but having graphical issues.

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
-    pygame.mixer.quit()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("My Pygame Window")
     clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    updatable.add(player)
-    drawable.add(player)
-
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+
     asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
 
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
-        dt = clock.tick(60) / 1000
-
-        for obj in updatable:
-            obj.update(dt)
-
-        screen.fill((0, 0, 0))
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit() 
 
         for obj in drawable:
-            obj.draw(screen)
+           obj.draw(screen)       
 
-        pygame.display.flip()
+        pygame.display.flip()        
 
-    pygame.quit()
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
